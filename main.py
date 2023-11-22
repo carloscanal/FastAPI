c91f72f57e6648db573e5ad36dcc2e2d4ff2d874
carloscanal <carloscanal@uma.es>
2023-11-22 19:35:13 +0100

    Initial

---
+++ main.py
@@ -0,0 +1,110 @@
+
+        
+from fastapi import FastAPI, Response               # FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # CORS, permitir origenes como swagger.io
+from typing import Union                            # typing, anotacionesiones de tipos
+from pydantic import BaseModel                      # pydantic, comprobaciones de tipos en runtime; tipos complejos
+
+class Item(BaseModel):
+    nombre: str
+    descripcion: Union[str, None] = None
+    precio: Union[int, None] = None
+
+
+# ejecutar con    python -m uvicorn main:api --reload --port 8000
+
+
+api = FastAPI()
+
+origins = [
+    "https://editor.swagger.io"                     # para probar con OpenAPI desde Swagger
+]
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+path = "/api/v1/"
+
+
+# 1. GET a un path (sin variables)
+
+@api.get(path + "hola")                             # endpoint: /api/v1/hola
+async def hola():                                   # o simplemente con def
+    return {"message": "Hola, mundo"}
+
+
+
+# 2. GET con variables de path: nombre
+
+@api.get(path + "hola/{nombre}")                    # endpoint: api/v1/hola/{nombre}    (Parámetro de Path)
+async def hola(nombre : str):
+    return {"message": "Hola, " + nombre}
+
+
+path = "/api/v2/"                                   # evita colision de paths (/hola)
+
+
+# 3. GET con parámetros de query opcionales: nombre
+
+@api.get(path + "hola")                      
+async def hola(nombre : Union[str, None] = None):   # endpoint: api/v2/hola?nombre=<nombre>
+    if nombre == None :                             #     p.ej. api/v2/hola?nombre=Pepe
+        nombre = "mundo"                              
+    return {"message": "Hola, " + nombre}
+
+
+
+# 4. GET con variables de path y query: nombre, saludo
+
+@api.get(path + "hola/{nombre}")                      
+async def hola(nombre : str, saludo : Union[str, None] = None):
+
+    if saludo != None :                            # endpoint: api/v2/hola/{nombre}?saludo=<saludo>
+        saludo = ", " + saludo                     #     p.ej. api/v2/hola/Pepe?saludo=qué%20tal%20estás?
+    else:
+        saludo = ""
+
+    return {"message": "Hola, " + nombre + saludo}
+
+
+path = "/api/v3/"
+
+# 5. POST con objeto nuevo en el body; código de estado por defecto
+
+@api.post(path + "items", status_code=201)         # response status code por defecto 
+async def create_item(item: Item):
+    
+    return {"message": item.nombre + " nos dice lo siguiente: " + item.descripcion + ", por " + str(item.precio) + " euros."}
+
+
+
+# 6. PUT con objeto actualizado en el body
+
+@api.put(path + "items/{id}")          
+async def update_item(id: int, item: Item):
+    item.precio += 10
+    return item
+
+
+
+# 7. DELETE, con código de estado condicionado
+
+@api.delete(path + "items/{id}")          
+async def remove_item(id: int, response : Response):
+    response.status_code=404                        # cambio el status code dependiendo de alguna condicion
+    return {"message": "Item no encontrado" }
+    
+    
+    
+    
+    
+    
+    
+    
+
+
