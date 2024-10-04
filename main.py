a1dab3cd988e4f6cb39993522b4445181f553845
carloscanal <carloscanal@uma.es>
2024-10-04 14:19:05 +0200

    Update 2024/10

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from fastapi.middleware.cors import CORSMiddleware  # CORS, permitir origenes co
 from typing import Union                            # typing, anotacionesiones de tipos
 from pydantic import BaseModel                      # pydantic, comprobaciones de tipos en runtime; tipos complejos
 
+
 class Item(BaseModel):
-    nombre: str
+    nombre: str = "Anónimo"
     descripcion: Union[str, None] = None
-    precio: Union[int, None] = None
+    precio: int = 0
 
 
 # ejecutar con    python -m uvicorn main:api --reload --port 8000
@@ -46,10 +47,10 @@ async def hola(nombre : str):
     return {"message": "Hola, " + nombre}
 
 
-path = "/api/v2/"                                   # evita colision de paths (/hola)
-
 
 # 3. GET con parámetros de query opcionales: nombre
+
+path = "/api/v2/"                                   # evita colision de paths cuando no hay parámetro de query (/hola)
 
 @api.get(path + "hola")                      
 async def hola(nombre : Union[str, None] = None):   # endpoint: api/v2/hola?nombre=<nombre>
@@ -57,30 +58,35 @@ async def hola(nombre : Union[str, None] = None):   # endpoint: api/v2/hola?nomb
         nombre = "mundo"                              
     return {"message": "Hola, " + nombre}
 
+# 3.1. Alternativa mñas sencilla
 
+path = "/api/v3/"
+
+@api.get(path + "hola")                      
+async def hola(nombre : str = "mundo"):             # endpoint: api/v3/hola?nombre=<nombre>; "mundo" como valor por defecto                      
+    return {"message": "Hola, " + nombre}
 
 # 4. GET con variables de path y query: nombre, saludo
 
 @api.get(path + "hola/{nombre}")                      
 async def hola(nombre : str, saludo : Union[str, None] = None):
 
-    if saludo != None :                            # endpoint: api/v2/hola/{nombre}?saludo=<saludo>
-        saludo = ", " + saludo                     #     p.ej. api/v2/hola/Pepe?saludo=qué%20tal%20estás?
+    if saludo != None :                            # endpoint: api/v3/hola/{nombre}?saludo=<saludo>
+        saludo = ", " + saludo                     #     p.ej. api/v3/hola/Pepe?saludo=qué%20tal%20estás?
     else:
         saludo = ""
 
     return {"message": "Hola, " + nombre + saludo}
 
 
-path = "/api/v3/"
+path = "/api/v4/"
 
 # 5. POST con objeto nuevo en el body; código de estado por defecto
 
 @api.post(path + "items", status_code=201)         # response status code por defecto 
 async def create_item(item: Item):
     
-    return {"message": item.nombre + " nos dice lo siguiente: " + item.descripcion + ", por " + str(item.precio) + " euros."}
-
+    return item
 
 
 # 6. PUT con objeto actualizado en el body
@@ -89,7 +95,6 @@ async def create_item(item: Item):
 async def update_item(id: int, item: Item):
     item.precio += 10
     return item
-
 
 
 # 7. DELETE, con código de estado condicionado
